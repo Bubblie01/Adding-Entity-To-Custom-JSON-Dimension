0ee590569afd548c54917c42a2258284b50ca776
rohan <manrohesh@gmail.com>
2022-03-10 15:35:18 -0500

    Forgot the registry lmao

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -16,7 +16,7 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		FunnySkeleton.registerFunnySkeleton();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
